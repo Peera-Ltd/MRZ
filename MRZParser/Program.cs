9ad4d1cda18d0882df0e40a45781c736fec93a1b
Stefan Novak <stefan@peera.co.uk>
2021-12-14 21:26:34 +0000

    feat: updated Main in Program.cs to loop through samples of MRZs

--- MRZParser/Program.cs
+++ MRZParser/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using MRZParser.Models;
 using MRZParser.Tests.Helpers;
 
 namespace MRZParser
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var samples = new List<string>
             {
@@ -21,14 +20,14 @@ namespace MRZParser
 
             foreach (var model in samples.Select(MRZParser.Parse))
             {
-                Console.WriteLine(model?.DocumentType);
-                Console.WriteLine(model?.CountryCode);
-                Console.WriteLine(model?.DocumentNumber);
-                Console.WriteLine(model?.DateOfBirth);
-                Console.WriteLine(model?.Sex);
-                Console.WriteLine(model?.ExpiryDate);
-                Console.WriteLine(model?.LastName);
-                Console.WriteLine(model?.FirstName);
+                Console.WriteLine($"Document Type: {model?.DocumentType}");
+                Console.WriteLine($"Country Code: {model?.CountryCode}");
+                Console.WriteLine($"Document Number: {model?.DocumentNumber}");
+                Console.WriteLine($"Date of Birth: {model?.DateOfBirth}");
+                Console.WriteLine($"Sex: {model?.Sex}");
+                Console.WriteLine($"Expiry Date: {model?.ExpiryDate}");
+                Console.WriteLine($"Last Name: {model?.LastName}");
+                Console.WriteLine($"First Name: {model?.FirstName}");
                 Console.WriteLine();
             }
         }
